4342c660ebd76ecd26bc57313fff960fed728282
Wesley de Paiva <107816080+wesleydepaiva@users.noreply.github.com>
2025-04-17 18:00:50 -0300

    cors

--- backend/api/gerar-planilha.js
+++ backend/api/gerar-planilha.js
@@ -2,6 +2,15 @@ import { gerarPlanilha } from "../services/planilhaServices.js";
 import { Readable } from "stream";
 
 export default async function handler(req, res) {
+  // Habilita CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // responde ao preflight
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
